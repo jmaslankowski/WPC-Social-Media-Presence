55b241bfffff1845668daad4e3c2588e7ace5d02
jmaslankowski <32862488+jmaslankowski@users.noreply.github.com>
2017-12-19 09:26:21 +0100

    Exceptions handling.
    Exception when problems with Twitter connecting.

--- WP2_Step2b_CollectingTweetsByEnterprise.py
+++ WP2_Step2b_CollectingTweetsByEnterprise.py
@@ -15,24 +15,28 @@ access_token_secret = ''
 account_names=['jmaslankowski','GUS_stat']
 
 def get_all_tweets(screen_name):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    alltweets = []
-    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
-    alltweets.extend(new_tweets)
-    oldest = alltweets[-1].id - 1
-    while len(new_tweets) > 0:
-        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+    try:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+        alltweets = []
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200)
         alltweets.extend(new_tweets)
+    except:
+        print("Cannot authenticate with Twitter credentials. Check consumer keys.")
+    else:
         oldest = alltweets[-1].id - 1
-        print ("%s tweets downloaded and still downloading..." % len(alltweets))
-    # the output file is WP2_enterprise_... .csv where ... is the account name
-    with open('WP2_enterprise_%s.csv' % screen_name, 'w') as f:
-        for tweet in alltweets:
-            outtweets = str(tweet.text)+"\n"
-            f.write(outtweets)
-    pass
+        while len(new_tweets) > 0:
+            new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+            alltweets.extend(new_tweets)
+            oldest = alltweets[-1].id - 1
+            print ("%s tweets downloaded and still downloading..." % len(alltweets))
+        # the output file is WP2_enterprise_... .csv where ... is the account name
+        with open('WP2_enterprise_%s.csv' % screen_name, 'w') as f:
+            for tweet in alltweets:
+                outtweets = str(tweet.text)+"\n"
+                f.write(outtweets)
+        pass
 if __name__ == '__main__':
     for account in account_names:
         get_all_tweets(account)
